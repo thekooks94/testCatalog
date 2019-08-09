c9a4c730cedfc2a971d1354c6446d27e6d782874
Roberto Spaziani <roberto.spaz@virgilio.it>
2019-08-09 15:12:50 +0200

    not found problem

--- WebCatalog/Global.asax.cs
+++ WebCatalog/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -15,6 +16,23 @@ namespace WebCatalog
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error(Object sender, EventArgs e)
+        {
+            Exception ex = Server.GetLastError();
+            HttpException httpException = ex as HttpException;
+
+            if (ex != null)
+            {
+                int errorCode = httpException.GetHttpCode();
+
+                if (errorCode == 404)
+                {
+
+                    Response.Redirect("~/");
+                }
+            }
         }
     }
 }
